1fd11dd5018b468fce18a80bb4f24efb6d59f8bc
Hong <shbinn23@gmail.com>
2023-01-15 01:36:58 +0900

    commit test

--- src/main/java/study/hong/order/OrderServiceImpl.java
+++ src/main/java/study/hong/order/OrderServiceImpl.java
@@ -17,6 +17,6 @@ public class OrderServiceImpl implements OrderService{
         Member member = memberRepository.findById(memberId);
         int discountPrice = discountPolicy.discount(member, itemPrice);
 
-        return new Order(memberId, itemName, itemPrice, discountPrice);
+        return new Order(memberId, itemName, itemPrice, discountPrice); // git test
     }
 }

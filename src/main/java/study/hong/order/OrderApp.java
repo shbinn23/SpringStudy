1bff7181ba1a9ef1b2a54adba1c8d1c72f009f6e
Hong <shbinn23@gmail.com>
2023-01-16 03:01:56 +0900

    commit test

--- src/main/java/study/hong/order/OrderApp.java
+++ src/main/java/study/hong/order/OrderApp.java
@@ -31,6 +31,6 @@ public class OrderApp {
 
         Order order = orderService.createOrder(memberId, "itemA", 10000);
 
-        System.out.println("order =" + order);
+        System.out.println("order =" + order);  // git test
     }
 }
